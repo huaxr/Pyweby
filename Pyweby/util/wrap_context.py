3fa808af2fd26f2036a9cb7c58e24c4c3503c3c5
敬贤。 <787518771@qq.com>
2018-08-15 19:22:52 +0800

    Delete wrap_context.py

--- Pyweby/util/wrap_context.py
+++
@@ -1,7 +0,0 @@
-def wrap(fn):
-    if fn is None or hasattr(fn, '_wrapped'):
-        return fn
-    def null_wrapper(*args, **kwargs):
-        return fn(*args, **kwargs)
-    null_wrapper._wrapped = True
-    return null_wrapper
